1797ed81a555c440f3d3cacaaf9f4158f564d612
Mkomarr <48152102+Mkomarr@users.noreply.github.com>
2019-04-01 09:34:02 +0200

    Update 3a-part-addTask.js

--- 3a-part-addTask.js
+++ 3a-part-addTask.js
@@ -1,3 +1,4 @@
+const toDoList = [];
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h1 span');
@@ -5,8 +6,12 @@ const listItems = document.getElementsByClassName('task');
 const input = document.querySelector('input');
 
 const removeTask = (e) => {
-    e.target.parentNode.remove();
+    //e.target.parentNode.remove();
+    const index = e.target.parentNode.dataset.key;
+    toDoList.splice(index, 1); //usuwanie z tablicy elementu z danym indexem, wymagane, żeby tablica miała zawsze aktualną zawartość, ale jest problem z indexami, bo one się nie aktualizuja po usunięciu - data-key nie odpowiada indexowi, potrzebna jest więc znów stworzona nowa tablica
     taskNumber.textContent = listItems.length; //zliczanie wpisanych zadań
+    //twrzenie nowej tablicy
+    renderList();
 }
 
 const addTask = (e) => {
@@ -21,6 +26,12 @@ const addTask = (e) => {
     ul.appendChild(newTask);
     input.value = ''; //czyszczenie input po dodaniu taska
 
+    //dodanie tablicy, żeby przechowywała nowe elementy, uwaga jednak po usunięciu buttonem "usuń" tablica nadal będzie przechowywała elementy
+    toDoList.push(newTask);
+
+    //nadawanie id dla elementu w tablicy żeby poznac jego index
+    renderList
+
     //liczenie elementów w liście zadań
    //const liItems = document.querySelectorAll('li.task').length;
    //taskNumber.textContent = liItems.length;
@@ -28,6 +39,14 @@ const addTask = (e) => {
 
    //odwołanie do buttona w konkretnym elemencie, który został stworzony
    newTask.querySelector('button').addEventListener('click', removeTask);
+}
+
+const renderList = () => {
+    ul.textContent = ""; //czyszczenie listy i generowanie jej od nowa za każdym razem gdy coś się dodaje
+     toDoList.forEach((toDoElement, key) => {
+        toDoElement.dataset.key = key; //dodanie atrybutu z indeksem tablicy
+        ul.appendChild(toDoElement)
+     });
 }
 
 form.addEventListener('submit', addTask);
